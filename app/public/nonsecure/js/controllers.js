12c6708842fb4f53eeff675240e770e02f2059e7
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-03-21 11:18:16 +0200

    fixed menu collapsing

--- app/public/nonsecure/js/controllers.js
+++ app/public/nonsecure/js/controllers.js
@@ -3,9 +3,29 @@
 /* Controllers */
 
 angular.module('divvd.controllers', []).
-  controller('MyCtrl1', [function() {
+controller('MyCtrl1', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+}]).
+controller('MyCtrl2', [function() {
 
-  }]);
+}]).
+controller('CollapseCtrl', ['$scope', '$document',
+    function($scope, $document) {
+  $scope.isCollapsed = true;
+  // ClickStatus indicates whether menu should be collapsed.
+  // If the click event reaches document click handler with clickStatus unset
+  // (null), menu will be collapsed.
+  $scope.clickStatus = null;
+  $scope.collapse = function(arg) {
+    $scope.clickStatus = arg;
+  };
+  $document.bind('click', function() {
+    if ($scope.clickStatus === null) {
+      $scope.isCollapsed = true;
+    } else {
+      $scope.isCollapsed = $scope.clickStatus;
+    }
+    $scope.clickStatus = null;
+    $scope.$digest();
+  });
+}]);
